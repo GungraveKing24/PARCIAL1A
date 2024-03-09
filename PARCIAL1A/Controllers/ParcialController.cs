664b5e925c359b6bcca6554795c1b3e23cc8eeee
Fernando Herrera <u.gomezherrera@gmail.com>
2024-03-09 09:26:06 -0600

    Frankdfghj

--- PARCIAL1A/Controllers/ParcialController.cs
+++ PARCIAL1A/Controllers/ParcialController.cs
@@ -192,7 +192,6 @@ namespace PARCIAL1A.Controllers
 
 
 
-
         //Metodos para Posts
 
         /// <summary>
@@ -213,69 +212,7 @@ namespace PARCIAL1A.Controllers
 
             return Ok(listadoPosts);
         }
-
-        /// <summary>
-        /// Actualizar Posts por ID
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        /// 
-
-        [HttpGet]
-        [Route("GetAllAutores")]
-        public IActionResult GetAutores()
-        {
-            List<Autores> listadoAutores = (from e in _parcialContext.Autores
-                                           select e).ToList();
-            if (listadoAutores.Count == 0)
-            {
-                return NotFound();
-            }
-
-            return Ok(listadoAutores);
-        }
-
-        [HttpGet]
-        [Route("GetAllAutorLibros")]
-        public IActionResult GetAutorLibros()
-        {
-            List<AutorLibro> listadoAutorLibro = (from e in _parcialContext.AutorLibro
-                                           select e).ToList();
-            if (listadoAutorLibro.Count == 0)
-            {
-                return NotFound();
-            }
-
-            return Ok(listadoAutorLibro);
-        }
-
-        [HttpGet]
-        [Route("GetAllLibros")]
-        public IActionResult GetLibros()
-        {
-            List<Libros> listadoLibros = (from e in _parcialContext.Libros
-                                           select e).ToList();
-            if (listadoLibros.Count == 0)
-            {
-                return NotFound();
-            }
-
-            return Ok(listadoLibros);
-        }
-
-        [HttpGet]
-        [Route("GetAllPosts")]
-        public IActionResult GetPosts()
-        {
-            List<Posts> listadoPosts = (from e in _parcialContext.Posts
-                                           select e).ToList();
-            if (listadoPosts.Count == 0)
-            {
-                return NotFound();
-            }
-
-            return Ok(listadoPosts);
-        }
+        
 
     }
 }
